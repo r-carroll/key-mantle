745362f2108b54313180838e6d33017dac8ddc2a
Ryan Carroll <ryan8440@gmail.com>
2019-03-10 16:19:21 -0400

    componetizing navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,7 @@ import { Component, OnInit, ViewChild, HostListener, ElementRef } from '@angular
 })
 export class AppComponent implements OnInit {
   title = 'K&M';
-  @ViewChild('mainNav') mainNav: ElementRef;
 
   ngOnInit() {
-  }
-
-  @HostListener('window:scroll', [])
-  onScroll(event) {
-    if (window.pageYOffset > 200 && this.mainNav && this.mainNav.nativeElement.classList) {
-      this.mainNav.nativeElement.classList.add('navbar-shrink');
-    } else if (this.mainNav && this.mainNav.nativeElement.classList) {
-      this.mainNav.nativeElement.classList.remove('navbar-shrink');
-    }
   }
 }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  navbarOpen = false;
+  @ViewChild('mainNav') mainNav: ElementRef;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(event) {
+    if (window.pageYOffset > 200 && this.mainNav && this.mainNav.nativeElement.classList) {
+      this.mainNav.nativeElement.classList.add('navbar-shrink');
+    } else if (this.mainNav && this.mainNav.nativeElement.classList) {
+      this.mainNav.nativeElement.classList.remove('navbar-shrink');
+    }
+  }
+
+}
